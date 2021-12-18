6ff48b231b0d92a39bad0cd97ec85eb26b6facc3
zcc <80560633+ZCC212@users.noreply.github.com>
2021-12-18 13:18:29 +0800

    通讯录

---
+++ test_contact.c
@@ -0,0 +1,53 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include"contact.h"
+void menu()
+{
+	printf("*********************************\n");
+	printf("**** 1.add        2.del     *****\n");
+	printf("**** 3.search     4.modify  *****\n");
+	printf("**** 5.show       6.sort    *****\n");
+	printf("**** 0.exit                 *****\n");
+	printf("*********************************\n");
+}
+int main()
+{
+	int input = 0;
+	
+	struct Contact con;//con就是通讯录，里面包含：1000的元素的数和size
+	//初始化通讯录
+	InitContact(&con);
+	do {
+		menu();
+		printf("请输入:>>");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			AddContact(&con);
+			break;
+		case 2:
+			DelContact(&con);
+			break;
+		case 3:
+			SearchContact(&con);
+			break;
+		case 4:
+			ModifyContact(&con);
+			break;
+		case 5:
+			ShowContact(&con);
+			break;
+		case 6:
+			SortContact(&con);
+			break;
+		case 0:
+			printf("退出通讯录！\n");
+			break;
+		default:
+			printf("选择错误，请重新输入！\n");
+			break;
+		}
+		
+	} while (input);
+	return 0;
+}
